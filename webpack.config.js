645fbf95ec000afa7deb43c1d93c2c71644730a1
Rabia Özcelik <52240205+rozcelik@users.noreply.github.com>
2023-06-14 05:29:23 +0300

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	// devtool: 'source-map',
+	entry: './lib/index.js',
+	mode: 'production',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		libraryTarget: 'umd',
+		library: 'phone',
+		globalObject: 'this'
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new MinifyPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: [
+					path.resolve(__dirname, 'lib')
+				],
+				loader: 'babel-loader',
+				options: {
+					babelrc: false,
+					presets: [
+						['env', {
+							targets: {
+								browsers: '>1%',
+								node: '6.10'
+							},
+							modules: false,
+							debug: true
+						}]
+					]
+				}
+			}
+		]
+	}
+};
